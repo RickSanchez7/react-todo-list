b75f9941cb95cbbc6e300ea5ca7fa100ecb85e74
RickSanchez7 <61505211+RickSanchez7@users.noreply.github.com>
2020-05-22 22:02:51 +0100

    finito

--- src/App.js
+++ src/App.js
@@ -6,33 +6,57 @@ import TodoList from "./components/TodoList";
 //
 class App extends Component {
   state = {
-    items: [
-      { id: 1, title: "wake up" },
-      { id: 2, title: "make breakfast" },
-    ],
+    items: [],
     id: uuidv4(),
     item: "",
     editItem: false,
   };
 
   handleChange = (e) => {
-    console.log("handleChange");
+    // e.preventDefault();
+    this.setState({
+      item: e.target.value,
+    });
   };
 
   handleSubmit = (e) => {
-    console.log("handleSubmit");
+    e.preventDefault();
+    const newItem = {
+      id: this.state.id,
+      title: this.state.item,
+    };
+    const updatedItems = [...this.state.items, newItem];
+
+    this.setState({
+      items: updatedItems,
+      item: "",
+      id: uuidv4(),
+      editItem: false,
+    });
   };
 
   clearList = () => {
-    console.log("clearList");
+    this.setState({
+      items: [],
+    });
   };
 
   handleDelete = (id) => {
-    console.log(`handleDelete ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    this.setState({
+      items: filteredItems,
+    });
   };
 
   handleEdit = (id) => {
-    console.log(`handleEdit ${id}`);
+    const filteredItems = this.state.items.filter((item) => item.id !== id);
+    const selectedItem = this.state.items.find((item) => item.id === id);
+    this.setState({
+      items: filteredItems,
+      item: selectedItem.title,
+      id: id,
+      editItem: true,
+    });
   };
 
   render() {
